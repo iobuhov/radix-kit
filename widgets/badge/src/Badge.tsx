d53859e1e755e8012fe0fa12dd3194ce7c12853d
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-11 19:03:08 +0100

    add badge

---
+++ widgets/badge/src/Badge.tsx
@@ -0,0 +1,8 @@
+import { ReactElement, createElement } from "react";
+import { Badge as BadgeView } from "@radix-ui/themes/components/badge";
+
+import "./ui/Badge.css";
+
+export function Badge(): ReactElement {
+    return <BadgeView>In progress</BadgeView>;
+}
