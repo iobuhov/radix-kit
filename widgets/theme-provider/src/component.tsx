0ba089048d60585bd33486de670ab73d9a44991c
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-13 17:29:48 +0100

    start with colors

--- widgets/theme-provider/src/component.tsx
+++ widgets/theme-provider/src/component.tsx
@@ -1,6 +1,7 @@
-import { createElement } from "react";
+import { createElement, useState } from "react";
 import { ThemeProps, Theme as RadixTheme } from "@radix-ui/themes-radix-kit/components/theme";
 
 export function Theme(props: ThemeProps) {
-    return <RadixTheme {...props} />;
+    const [isDark] = useState(() => window.matchMedia("(prefers-color-scheme: dark)").matches);
+    return <RadixTheme {...props} appearance={isDark ? "dark" : "light"} />;
 }
