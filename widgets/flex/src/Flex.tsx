62fc8be9fb53faca5a60c12782cb939be2cff019
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-17 19:13:10 +0100

    add flex widget

---
+++ widgets/flex/src/Flex.tsx
@@ -0,0 +1,7 @@
+import { ReactElement, createElement } from "react";
+import { Flex as RadixFlex } from "@radix-ui/themes-radix-kit/components/flex";
+import { FlexContainerProps } from "../typings/FlexProps";
+
+export function Flex(props: FlexContainerProps): ReactElement {
+    return <RadixFlex className={props.class} style={props.style} wrap="wrap" children={props.children} />;
+}
