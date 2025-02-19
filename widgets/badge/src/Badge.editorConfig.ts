a720bf188c90bdf1bd5d827266f3d5896b7b63ec
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-02-19 17:30:09 +0100

    update badge

--- widgets/badge/src/Badge.editorConfig.ts
+++ widgets/badge/src/Badge.editorConfig.ts
@@ -1,10 +1,6 @@
-import { hidePropertyIn } from "@mendix/pluggable-widgets-tools";
 import { BadgePreviewProps } from "../typings/BadgeProps";
 import { Properties } from "../typings/editor-types";
 
-export function getProperties(values: BadgePreviewProps, schema: Properties): Properties {
-    if (values.size !== "object") {
-        hidePropertyIn(schema, values, "sizeInitial");
-    }
+export function getProperties(_values: BadgePreviewProps, schema: Properties): Properties {
     return schema;
 }
--- widgets/badge/src/map-prop.ts
+++ widgets/badge/src/map-prop.ts
@@ -1,16 +1,5 @@
 import { BadgeProps } from "@radix-ui/themes-radix-kit/components/Badge";
-import { BadgeContainerProps, BadgePreviewProps, SizeEnum } from "../typings/BadgeProps";
-
-const sizeMap: Record<SizeEnum, "1" | "2" | "3" | undefined> = {
-    s1: "1",
-    s2: "2",
-    s3: "3",
-    object: undefined
-};
-
-export const size = (size: SizeEnum): BadgeProps["size"] => {
-    return size === "object" ? undefined : sizeMap[size];
-};
+import { BadgeContainerProps, BadgePreviewProps } from "../typings/BadgeProps";
 
 interface CommonProps {
     text: string;
